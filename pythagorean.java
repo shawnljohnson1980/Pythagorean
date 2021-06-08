d91a8c8494db3e7dd3bcbe71d105448943b3198e
Shawn Johnson <78113657+shawnljohnson1980@users.noreply.github.com>
2021-06-08 14:16:25 -0400

    Add files via upload

---
+++ pythagorean.java
@@ -0,0 +1,7 @@
+public class Pythagorean {
+    public double calculateHypotenuse(int legA, int legB) {
+        double four = 4.0;
+        double squareRoot = Math.sqrt(four);
+        return four;
+    }
+}
